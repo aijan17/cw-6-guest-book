d745fdda536740a951d51209cd5111440d089b17
Aijan Asilbekova <aizhan.asilbekova@gmail.com>
2021-03-06 18:04:51 +0600

    added migrations modified admin.py added models Guest

--- guest_book/webapp/admin.py
+++ guest_book/webapp/admin.py
@@ -2,4 +2,5 @@ from django.contrib import admin
 
 from webapp.models import Guest
 
+
 admin.site.register(Guest)
--- guest_book/webapp/models.py
+++ guest_book/webapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 status_choice = (
     ('active', 'Активно'),
     ('blocked', 'Заблокировано')
@@ -10,10 +11,11 @@ class Guest(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100, null=False, blank=False)
     email = models.EmailField(max_length=100, blank=False, null=False)
-    text = models.TextField(max_length=300, blank=False, null=False)
-    cread_date = models.DateField(auto_now_add=True)
-    update_date = models.DateField(auto_now=True)
+    text = models.TextField(max_length=400, blank=False, null=False)
+    cread_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=100, blank=False, null=False, default='active', choices=status_choice)
 
     def __str__(self):
         return '{}: {}'.format(self.id, self.name, self.email, self.text)
+
